862cf7cb90d6e75596215783939589defcfbe2af
Umaima <umaimarauf2005@gmail.com>
2024-08-12 13:31:56 +0500

    Prob: 771.py

---
+++ Problems/Prob: 771.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        lst=[i for i in stones if i in jewels]
+        return len(lst)
+        
